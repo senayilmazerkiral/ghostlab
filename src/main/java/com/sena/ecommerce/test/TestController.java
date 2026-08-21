220a1a75e9c0a3b46ce931773244dd4541d94d51
Sena <Sena21yilmaz@gmail.com>
2026-08-21 23:08:56 +0300

    Add GhostLab API home endpoint

--- src/main/java/com/sena/ecommerce/test/TestController.java
+++ src/main/java/com/sena/ecommerce/test/TestController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    @GetMapping("/")
+    public String home() {
+        return "GhostLab API is running!";
+    }
+
     @GetMapping("/test")
     public String test() {
         return "JWT protected endpoint works!";
